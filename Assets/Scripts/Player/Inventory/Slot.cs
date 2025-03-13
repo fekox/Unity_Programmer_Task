adf3fb8e5f39a952bb62d569a00e838820caef90
fekox <facusantos123@gmail.com>
2025-03-12 22:00:59 -0300

    Add the item descriptions

--- Assets/Scripts/Player/Inventory/Slot.cs
+++ Assets/Scripts/Player/Inventory/Slot.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Image image;
     public TextMeshProUGUI _name;
     public TextMeshProUGUI description;
 
+    public GameObject descriptionSign;
+
     public bool isEmpty = true;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!isEmpty)
+        {
+            descriptionSign.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        descriptionSign.SetActive(false);
+    }
 }
